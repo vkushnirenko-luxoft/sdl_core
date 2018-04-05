f06ce5e0c192ed270b877db192a7475bea7e7909
Vadym Kushnirenko (GitHub) <32477371+vkushnirenko-luxoft@users.noreply.github.com>
2018-04-05 12:31:02 +0300

    back dummy

--- src/components/connection_handler/test/connection_handler_impl_test.cc
+++ src/components/connection_handler/test/connection_handler_impl_test.cc
@@ -1087,8 +1087,6 @@ TEST_F(ConnectionHandlerTest, StartService_withServices_withParams) {
   uint32_t start_video = 0;
   // create a dummy pointer
   int dummy = 0;
-  int y = 77;
-  int z = y/dummy;
   std::vector<std::string> empty;
   BsonObject* dummy_param = reinterpret_cast<BsonObject*>(&dummy);
   connection_handler_->set_protocol_handler(&mock_protocol_handler_);
